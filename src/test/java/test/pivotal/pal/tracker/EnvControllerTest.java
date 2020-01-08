44059328fd8109feb45bbdeb6f212985533bfff1
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:04 -0500

    lab1

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,5 +1,6 @@
 package test.pivotal.pal.tracker;
 
+//import io.pivotal.pal.tracker.EnvController;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -10,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
-        EnvController controller = new EnvController(
+       EnvController controller = new EnvController(
             "8675",
             "12G",
             "34",
@@ -23,6 +24,7 @@ public class EnvControllerTest {
         assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
         assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
         assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
-    }
 
+
+    }
 }
